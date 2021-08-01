005a89e72b1b1c378ce2949952113f589b4f97e0
Surya <54676684+gpsurya@users.noreply.github.com>
2021-08-01 16:15:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ ydl_opts = {
 ydl = YoutubeDL(ydl_opts)
 links=[]
 finalurl=""
-STREAM=os.environ.get("STREAM_URL", "https://eu10.fastcast4u.com/clubfmuae")
+STREAM=os.environ.get("STREAM_URL", "https://node-31.zeno.fm/4g65yz9gg0quv.aac?rj-ttl=5&rj-tok=AAABdtuvkw4A9ORsR7htLe8_1Q")
 regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
 match = re.match(regex,STREAM)
 if match:
@@ -38,18 +38,18 @@ else:
     finalurl=STREAM
 
 class Config:
-    ADMIN = os.environ.get("ADMINS", '')
+    ADMIN = os.environ.get("ADMINS", '709294532')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    API_ID = int(os.environ.get("API_ID", 7670903))
+    CHAT = int(os.environ.get("CHAT", "-1001215436654"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001215436654")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
     STREAM_URL=finalurl
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "N")
-    ARQ_API=os.environ.get("ARQ_API", "")
+    ARQ_API=os.environ.get("ARQ_API", "MWSEUQ-ONSQUT-VVDWHA-PUCXHR-ARQ")
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
     if REPLY_MESSAGE:
         REPLY_MESSAGE=REPLY_MESSAGE
@@ -60,8 +60,8 @@ class Config:
         EDIT_TITLE=None
     DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
     DELAY = int(os.environ.get("DELAY", 10))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_HASH = os.environ.get("API_HASH", "518da36fc2b883e74036028ac6c6d854")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1914403790:AAFBfFSp7sldc8E1SqfjbApkrcFlk0NiQt8") 
+    SESSION = os.environ.get("SESSION_STRING", "BAAPVHhQtTIDBR_Nk9uuDUEMISWVi53ghirWho97vfLOKyhhBrV_Qlw7-w0J9gDzxV2I2MyDfFgnsJ6gXqh4UC1J_3y40MmQvNoLx-UhfBU2GwWQgUV9eQ_BVXMjFUeCwWNEdxzQFDwFMYqbKe625-XJ6WAVPM8aakas2I1if_1Onrgciu7tWroXJUMw6uXvl4VRIuNcMR9E3xNPtTVZiipyF8dqimezZvG52zr_uB1DcvAZyiTzvEhoFoK9-QDy3kwvvdO_e3oHoMWN4bN2MASbFcdAra8ggHmF6e6tImD6V41v6AMtNVMsm4f1Jy_QM9AF9A5Pt1sGYTV6ZelMr2jBc7YdvAA")
     playlist=[]
     msg = {}
